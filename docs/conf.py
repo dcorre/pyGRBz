d71043ffbd605447e39cc350872870debe726ca5
CORRE david <david.corre@lam.fr>
2018-06-13 17:58:17 -0500

    commented import in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ sys.path.append(os.path.abspath('sphinxext'))
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-#extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 extensions = [
           'matplotlib.sphinxext.mathmpl',
           'matplotlib.sphinxext.only_directives',
@@ -52,17 +52,17 @@ extensions = [
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.inheritance_diagram',
-    'sphinx.ext.mathjax',
+#extensions = [
+#    'sphinx.ext.autodoc',
+#    'sphinx.ext.autosummary',
+#    'sphinx.ext.intersphinx',
+#    'sphinx.ext.inheritance_diagram',
+#    'sphinx.ext.mathjax',
     #'sphinx.ext.linkcode',
-    'numpydoc',
-    'sphinxcontrib.programoutput',
+#    'numpydoc',
+#    'sphinxcontrib.programoutput',
     #matplotlib.sphinxext.plot_directive.__name__
-]
+#]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
