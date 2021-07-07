8f3d633f694dcfb5845cfc0bf764e5849bcb32f8
David Corre <david.corre@iap.fr>
2021-07-07 06:33:37 +0200

    add new scripts for computing model fluxes

---
+++ pyGRBz/fluxes.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# import time
+import numpy as np
+from pyGRBz.extinction_correction import sed_extinction
+
+
+def SPL_lc(t, F0, t0, norm, alpha):
+    """Temporal evolution as single power law for Light Curve"""
+    return norm * F0 * (t / t0) ** (-alpha)
+
+
+def BPL_lc(t, F0, norm, alpha1, alpha2, t1, s):
+    """Temporal evolution as broken power law for Light Curve"""
+    return (
+        norm
+        * F0
+        * ((t / t1) ** (-s * alpha1) + (t / t1) ** (-s * alpha2)) ** (-1.0 / s)
+    )
+
+
+def SPL_sed(wvl, F0, wvl0, norm, beta):
+    """Spectral evolution as single power law for SED"""
+    return norm * F0 * (wvl / wvl0) ** beta
+
+
+def BPL_sed(wvl, F0, norm, beta1, beta2, wvl1, s):
+    """Spectral evolution as broken power law for SED"""
+    return (
+        norm
+        * F0
+        * ((wvl / wvl1) ** (s * beta1) + (wvl / wvl1) ** (s * beta2)) ** (1.0 / s)
+    )
+
+
+def compute_model_flux(
+    wvl, F0, wvl0, norm, beta, z, Av, NHx, ext_law, Host_dust, Host_gas, igm_att
+):
+    """ Compute model flux over filter bands"""
+
+    Flux = SPL_sed(wvl, F0, wvl0, norm, beta) * sed_extinction(
+        wvl,
+        z,
+        Av,
+        NHx=NHx,
+        ext_law=ext_law,
+        Host_dust=Host_dust,
+        Host_gas=Host_gas,
+        igm_att=igm_att,
+    )
+    return Flux
+
+
+def compute_model_integrated_flux(
+    wavelength,
+    sys_response,
+    F0,
+    wvl0,
+    norm,
+    beta,
+    z,
+    Av,
+    NHx,
+    ext_law,
+    Host_dust,
+    Host_gas,
+    igm_att,
+):
+    """ Compute and integrate model flux over filter bands"""
+    # tt0 = time.time()
+    flux = compute_model_flux(
+        wavelength,
+        F0,
+        wvl0,
+        norm,
+        beta,
+        z,
+        Av,
+        NHx,
+        ext_law,
+        Host_dust,
+        Host_gas,
+        igm_att,
+    )
+
+    integrated_flux = np.trapz(flux * sys_response, x=wavelength, axis=1) / (
+        np.trapz(sys_response, x=wavelength, axis=1)
+    )
+
+    # dwvl = np.gradient(wavelength)
+    # dwvl = np.ediff1d(wavelength)
+    # dwvl = np.append(dwvl, dwvl[-1])
+    # integrated_flux = np.sum(flux[:-1] * sys_response[:,:-1] *dwvl, axis=1) / (
+    #        np.sum(sys_response[:,:-1]*dwvl, axis=1)
+    # )
+
+    # tt1 = time.time()
+
+    # print ("Summing flux in {:.2e}".format(tt1-tt0))
+    # print ("Summing flux 2 in {:.2e}".format(tt2-tt1))
+    return integrated_flux
---
+++ pyGRBz/fluxes_cy.pyx
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#cython: boundscheck=False, wraparound=False, initializedcheck=False, cdivision=True
+
+import numpy as np
+from pyGRBaglow import constants as cc
+from libc.math cimport pow, exp
+import cython
+from cython.parallel import prange, parallel
+from pyGRBaglow.reddening_cy import Pei92, gas_absorption
+from pyGRBaglow.igm_cy import meiksin
+
+
+cdef double SPL_sed_float(double wvl, double F0, double wvl0, double norm,
+                          double beta) nogil:
+    """Spectral evolution as single power law for SED"""
+    return norm * F0 * pow(wvl / wvl0, beta)
+
+
+cpdef SPL_sed(double[:] wvl, double F0, double wvl0, double norm, double beta):
+    """Spectral evolution as single power law for SED"""
+    cdef Py_ssize_t i
+    cdef Py_ssize_t N = len(wvl)
+    cdef double[:] flux = np.zeros(N, dtype=np.float64)
+    
+    for i in prange(N, nogil=True, num_threads=1):
+        flux[i] = SPL_sed_float(wvl[i], F0, wvl0, norm, beta)
+    return flux
+
+
+cdef double BPL_sed_float(double wvl, double F0, double norm, double beta1,
+                          double beta2, double wvl1, double s) nogil:
+    """Spectral evolution as broken power law for SED"""
+    cdef double flux
+    flux = norm * F0 * pow(pow(wvl / wvl1, s * beta1) + pow(wvl / wvl1, s * beta2), 1.0 / s)
+    
+    return flux
+
+
+cpdef BPL_sed(double[:] wvl, double F0, double norm, double beta1,
+              double beta2, double wvl1, double s):
+    """Spectral evolution as single power law for SED"""
+    cdef Py_ssize_t i
+    cdef Py_ssize_t N = len(wvl)
+    cdef double[:] flux = np.zeros(N, dtype=np.float64)
+    
+    for i in prange(N, nogil=True, num_threads=1):
+        flux[i] = BPL_sed_float(wvl[i], F0, norm, beta1, beta2, wvl1, s)
+    return flux
+
+
+cpdef compute_model_flux(
+    double[:] wvl,
+    double F0,
+    double wvl0,
+    double norm,
+    double beta,
+    double z,
+    double Av,
+    double NHx,
+    str ext_law="smc",
+    bint Host_dust=True,
+    bint Host_gas=True,
+    str igm_att="meiksin"
+):
+    cdef Py_ssize_t i
+    cdef Py_ssize_t N = len(wvl)
+    cdef double[:] flux = SPL_sed(wvl, F0, wvl0, norm, beta)
+    cdef double[:] Trans_igm = meiksin(wvl, z, unit="angstroms", Xcut=True)
+    cdef double[:] Trans_dust = np.empty(N, dtype=np.float64)
+    cdef double[:] Trans_gas = np.empty(N, dtype=np.float64)
+
+    if Host_dust:
+        Trans_dust = Pei92(wvl, Av, z, ext_law=ext_law, Xcut=True)[1]
+    if Host_gas:
+        Trans_gas = gas_absorption(wvl, z , NHx=NHx)
+    
+    for i in prange(N, nogil=True, num_threads=1):
+        flux[i] = flux[i] * Trans_igm[i]
+        if Host_dust:
+            flux[i] = flux[i] * Trans_dust[i]
+        if Host_gas:    
+            flux[i] = flux[i] * Trans_gas[i]
+    
+    return flux
+    
+
+def compute_model_integrated_flux(
+    double[:] wvl,
+    double[:, ::1] sys_response,
+    double F0,
+    double wvl0,
+    double norm,
+    double beta,
+    double z,
+    double Av,
+    double NHx,
+    str ext_law="smc",
+    bint Host_dust=True,
+    bint Host_gas=True,
+    str igm_att="meiksin"
+):
+    cdef Py_ssize_t i, j
+    cdef Py_ssize_t N = len(wvl)
+    cdef double dwvl
+    cdef Py_ssize_t N2 = sys_response.shape[0]
+    cdef double[:] flux_int = np.zeros(N2, dtype=np.float64)
+    cdef double[:] a=np.zeros(N2, dtype=np.float64)
+    cdef double[:] b=np.zeros(N2, dtype=np.float64)
+    cdef double[:] flux = compute_model_flux(wvl, F0, wvl0, norm, beta, z, Av, NHx,
+                                                 ext_law, Host_dust, Host_gas, igm_att
+                                                )
+
+    for i in prange(N-1, nogil=True, num_threads=1):
+        for j in range(N2):
+            dwvl = wvl[i+1]-wvl[i]
+            a[j] = a[j] + (sys_response[j,i+1] + sys_response[j,i])/2. * dwvl
+            b[j] = b[j] + (sys_response[j,i+1] * flux[i+1] + sys_response[j,i] * flux[i])/2. * dwvl
+        
+    for j in range(N2):
+        flux_int[j] = b[j] / a[j]
+
+    return flux_int
+
